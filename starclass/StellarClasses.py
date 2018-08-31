5a9c47236a305d6a0afdcc1ed7df3c7d82a59335
David Armstrong <davidarmstrong@Davids-iMac.wifi-campus.warwick.ac.uk>
2018-08-31 16:59:45 +0100

    re-inserting StellarClasses..

---
+++ starclass/StellarClasses.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Enum of all the possible stellar classes.
+.. codeauthor:: Rasmus Handberg <rasmush@phys.au.dk>
+"""
+
+from __future__ import division, print_function, with_statement, absolute_import
+import enum
+
+__docformat__ = 'restructuredtext'
+
+class StellarClasses(enum.Enum):
+	"""
+	Enum of all the possible stellar classes.
+	.. codeauthor:: Rasmus Handberg <rasmush@phys.au.dk>
+	"""
+
+	SOLARLIKE = 'solar'
+
+	TRANSIT = 'transit'
+
+	RRLYR = 'RR Lyrae'
+	CEPHEID = 'Cepheid'
+
+	DSCT = 'delta Scuti'
+	BCEP = 'beta Cepheid'
+
+	GDOR = 'gamma Doradus'
+	SPB = 'Slowly pulsating B star'
+
+	# Rapid pulsators:
+	ROAP = 'roAp'
+	SDB = 'sdB'
+	WD = 'White Dwarf'
+
+	TRANSIENT = 'transient'
+
+	SPOTS = 'Spot modulation'
+
+	LPV = 'Long period variable'
+
+	CONSTANT = 'constant'

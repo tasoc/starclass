ed5de0065185a1d160ec7187e9d2c0caba50d76d
Rasmus Handberg <rasmush@phys.au.dk>
2019-05-28 11:46:35 +0200

    Drop Python 2.7 support

--- starclass/utilities.py
+++ starclass/utilities.py
@@ -10,12 +10,12 @@ from __future__ import division, print_function, with_statement, absolute_import
 import six.moves.cPickle as pickle
 import gzip
 
-PICKLE_DEFAULT_PROTOCOL = 2 #: Default protocol to use for saving pickle files.
+PICKLE_DEFAULT_PROTOCOL = 4 #: Default protocol to use for saving pickle files.
 
 def savePickle(fname, obj):
 	"""
 	Save an object to file using pickle.
-	
+
 	Parameters:
 		fname (string): File name to save to. If the name ends in '.gz' the file
 			will be automatically gzipped.
@@ -29,21 +29,21 @@ def savePickle(fname, obj):
 
 	with o(fname, 'wb') as fid:
 		pickle.dump(obj, fid, protocol=PICKLE_DEFAULT_PROTOCOL)
-		
+
 
 def loadPickle(fname):
 	"""
 	Load an object from file using pickle.
-	
+
 	Parameters:
 		fname (string): File name to save to. If the name ends in '.gz' the file
 			will be automatically unzipped.
 		obj (object): Any pickalble object to be saved to file.
-		
+
 	Returns:
 		object: The unpickled object from the file.
 	"""
-	
+
 	if fname.endswith('.gz'):
 		o = gzip.open
 	else:

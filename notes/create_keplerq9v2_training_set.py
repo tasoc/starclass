b4a0259afde95af796c8565b57295145630be7c1
Rasmus Handberg <rasmush@phys.au.dk>
2020-05-11 14:14:00 +0200

    Updated notes

--- notes/create_keplerq9v2_training_set.py
+++ notes/create_keplerq9v2_training_set.py
@@ -24,28 +24,68 @@ if __name__ == '__main__':
 	# Directory where KeplerQ9 ASCII files are stored:
 	thisdir = r'E:\keplerq9\full'
 
+	# Where output will be saved:
+	output_dir = r'E:\keplerq9\keplerq9v2'
+
 	# Make sure directories exists:
-	os.makedirs('keplerq9v2', exist_ok=True)
-	shutil.copy('keplerq9v2_targets.txt', 'keplerq9v2/targets.txt')
+	os.makedirs(output_dir, exist_ok=True)
+	shutil.copy('keplerq9v2_targets_unique.txt', os.path.join(output_dir, 'targets.txt'))
 
-	starlist = np.genfromtxt('keplerq9v2/targets.txt', delimiter=',', dtype=None, encoding='utf-8')
+	starlist = np.genfromtxt(os.path.join(output_dir, 'targets.txt'), delimiter=',', dtype=None, encoding='utf-8')
 
-	with open('keplerq9v2/diagnostics.txt', 'w') as diag:
-		with tarfile.open(os.path.join(thisdir, 'Q09_public_ascii_20160905.tgz'), 'r') as tgz:
-			for starid, sclass in tqdm(starlist):
+	starlist_unique, cnt = np.unique(starlist, return_counts=True, axis=0)
+	if len(starlist) != len(starlist_unique):
+		print("Duplicate entries:")
+		for line in starlist_unique[cnt > 1]:
+			print('%s,%s' % tuple(line))
+
+	# Make sure that there are not duplicate starids:
+	us, cnt = np.unique(starlist[:, 0], return_counts=True)
+	if len(starlist) != len(us):
+		print("Duplicate entries:")
+		for s in us[cnt > 1]:
+			indx = (starlist[:, 0] == s)
+			for line in starlist[indx, :]:
+				print('%s,%s' % tuple(line))
+
+		raise Exception("%d duplicate starids" % (len(starlist) - len(us)))
+
+	with open(os.path.join(output_dir, 'diagnostics.txt'), 'w') as diag:
+		for starid, sclass in tqdm(starlist):
+			sclass = sclass.upper()
+			if starid.startswith('constant_') or starid.startswith('fakerrlyr_'):
+				fname_save = starid + '.txt'
+				starid = -1
+			else:
 				starid = int(starid)
-				sclass = sclass.upper()
+				fname_save = '{starid:09d}.txt'.format(starid=starid)
 
+			# The path to the
+			fpath_save = os.path.join(output_dir, sclass, fname_save)
+			#if os.path.exists(fpath_save):
+			#	continue
+
+			if starid == -1:
+				# Load file that should already exist:
+				data = np.loadtxt(fpath_save, usecols=(0,1,2))
+			else:
 				# Find the Kepler Q9 ASCII file for this target:
 				fname = 'kplr{starid:09d}-2011177032512_llc.dat'.format(
 					starid=starid
 				)
-				fpath = os.path.join(thisdir, fname)
-				print(fpath)
 
-				if not os.path.exists(fpath):
-					print("Extracting")
-					tgz.extract(fname, fpath)
+				subdir = '{0:09d}'.format(starid)[:5]
+				fpath = os.path.join(thisdir, subdir, fname)
+
+				# Move file into subdir, just to not have too many files in one directory:
+				if not os.path.exists(fpath) and os.path.exists(os.path.join(thisdir, fname)):
+					os.makedirs(os.path.join(thisdir, subdir), exist_ok=True)
+					shutil.move(os.path.join(thisdir, fname), fpath)
+
+				#if not os.path.exists(fpath):
+				#	print("Extracting " + fname)
+				#	with tarfile.open(os.path.join(thisdir, 'Q09_public_ascii_20160905.tgz'), 'r') as tgz:
+				#		tgz.extract(fname, fpath)
 
 				# Load Kepler Q9 ASCII file (PDC corrected):
 				data = np.loadtxt(fpath, usecols=(0,3,4))
@@ -71,20 +111,19 @@ if __name__ == '__main__':
 				#sys.exit()
 
 				# Save file:
-				fpath = os.path.join('keplerq9v2', sclass, '{starid:09d}.txt'.format(starid=starid))
-				os.makedirs(os.path.dirname(fpath), exist_ok=True)
-				np.savetxt(fpath, data, delimiter='  ', fmt=('%.8f', '%.16e', '%.16e'))
+				os.makedirs(os.path.dirname(fpath_save), exist_ok=True)
+				np.savetxt(fpath_save, data, delimiter='  ', fmt=('%.8f', '%.16e', '%.16e'))
 
-				# Calculate diagnostics:
-				variance = nanvar(data[:,1], ddof=1)
-				rms_hour = rms_timescale(data[:,0], data[:,1], timescale=3600/86400)
-				ptp = nanmedian(np.abs(np.diff(data[:,1])))
+			# Calculate diagnostics:
+			variance = nanvar(data[:,1], ddof=1)
+			rms_hour = rms_timescale(data[:,0], data[:,1], timescale=3600/86400)
+			ptp = nanmedian(np.abs(np.diff(data[:,1])))
 
-				# Add target to TODO-list:
-				diag.write("{variance:e},{rms_hour:e},{ptp:e}\n".format(
-					variance=variance,
-					rms_hour=rms_hour,
-					ptp=ptp
-				))
+			# Add target to TODO-list:
+			diag.write("{variance:.16e},{rms_hour:.16e},{ptp:.16e}\n".format(
+				variance=variance,
+				rms_hour=rms_hour,
+				ptp=ptp
+			))
 
 	print("DONE")

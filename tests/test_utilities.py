14973b61d0f7c93f53d338ad0d207673df995763
Rasmus Handberg <rasmush@phys.au.dk>
2020-09-25 23:37:33 +0200

    Fixed warning in test of rms_timescale

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -8,6 +8,7 @@ import pytest
 import os.path
 import numpy as np
 import tempfile
+import warnings
 from lightkurve import LightCurve
 import conftest # noqa: F401
 from starclass.utilities import savePickle, loadPickle, rms_timescale
@@ -59,7 +60,23 @@ def test_rms_timescale():
 
 	# Pure nan in the time-column should raise ValueError:
 	with np.testing.assert_raises(ValueError):
-		rms = rms_timescale(LightCurve(time=time*np.nan, flux=flux))
+		with warnings.catch_warnings():
+			warnings.filterwarnings('ignore', category=LightkurveWarning, message='LightCurve object contains NaN times')
+			rms = rms_timescale(LightCurve(time=time*np.nan, flux=flux))
+
+	# Time with invalid contents (e.g. Inf) should throw an ValueError:
+	time_invalid = time.copy()
+	time_invalid[2] = np.inf
+	with np.testing.assert_raises(ValueError):
+		rms = rms_timescale(LightCurve(time=time_invalid, flux=flux))
+
+	time_someinvalid = time.copy()
+	time_someinvalid[2] = np.nan
+	with warnings.catch_warnings():
+		warnings.filterwarnings('ignore', category=LightkurveWarning, message='LightCurve object contains NaN times')
+		rms = rms_timescale(LightCurve(time=time_someinvalid, flux=flux))
+	print(rms)
+	np.testing.assert_allclose(rms, 0)
 
 	# Test with timescale longer than timespan should return zero:
 	flux = np.random.randn(1000)

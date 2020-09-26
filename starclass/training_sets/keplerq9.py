b17dac2c35c4fbda42d32bcc0d937bfd18d4ea0e
Rasmus Handberg <rasmush@phys.au.dk>
2020-09-26 19:55:41 +0200

    Work on #39 and #43

--- starclass/training_sets/keplerq9.py
+++ starclass/training_sets/keplerq9.py
@@ -35,7 +35,11 @@ class keplerq9(TrainingSet):
 		self.input_folder = self.tset_datadir('keplerq9', 'https://tasoc.dk/pipeline/starclass_trainingsets/keplerq9.zip')
 
 		# Find the number of training sets:
-		data = np.genfromtxt(os.path.join(self.input_folder, 'targets.txt'), dtype=None, delimiter=',', encoding='utf-8')
+		data = np.genfromtxt(os.path.join(self.input_folder, 'targets.txt'),
+			dtype=None,
+			delimiter=',',
+			comments='#',
+			encoding='utf-8')
 		self.nobjects = data.shape[0]
 
 		# Initialize parent
@@ -100,34 +104,17 @@ class keplerq9(TrainingSet):
 
 		logger = logging.getLogger(__name__)
 
-		data = np.genfromtxt(os.path.join(self.input_folder, 'targets.txt'), dtype=None, delimiter=',', encoding='utf-8')
-
-		# Translation of Mikkel's identifiers into the broader:
-		translate = {
-			'SOLARLIKE': StellarClasses.SOLARLIKE,
-			'ECLIPSE': StellarClasses.ECLIPSE,
-			'RRLYR_CEPHEID': StellarClasses.RRLYR_CEPHEID,
-			'GDOR_SPB': StellarClasses.GDOR_SPB,
-			'DSCT_BCEP': StellarClasses.DSCT_BCEP,
-			'CONTACT_ROT': StellarClasses.CONTACT_ROT,
-			'APERIODIC': StellarClasses.APERIODIC,
-			'CONSTANT': StellarClasses.CONSTANT
-		}
+		data = np.genfromtxt(os.path.join(self.input_folder, 'targets.txt'),
+			dtype=None,
+			delimiter=',',
+			comments='#',
+			encoding='utf-8')
 
 		# Create list of all the classes for each star:
 		lookup = []
-		for rowidx,row in enumerate(data):
-			#starid = int(row[0][4:])
+		for rowidx, row in enumerate(data):
 			labels = row[1].strip().split(';')
-			lbls = []
-			for lbl in labels:
-				lbl = lbl.strip()
-				c = translate.get(lbl.strip())
-				if c is None:
-					logger.error("Unknown label: %s", lbl)
-				else:
-					lbls.append(c)
-
+			lbls = [StellarClasses[lbl.strip()] for lbl in labels]
 			if self.testfraction > 0:
 				if rowidx in self.train_idx:
 					lookup.append(tuple(set(lbls)))
@@ -143,35 +130,19 @@ class keplerq9(TrainingSet):
 
 		if self.testfraction <= 0:
 			return []
-		else:
-			data = np.genfromtxt(os.path.join(self.input_folder, 'targets.txt'), dtype=None, delimiter=',', encoding='utf-8')
 
-			# Translation of Mikkel's identifiers into the broader:
-			translate = {
-				'SOLARLIKE': StellarClasses.SOLARLIKE,
-				'ECLIPSE': StellarClasses.ECLIPSE,
-				'RRLYR_CEPHEID': StellarClasses.RRLYR_CEPHEID,
-				'GDOR_SPB': StellarClasses.GDOR_SPB,
-				'DSCT_BCEP': StellarClasses.DSCT_BCEP,
-				'CONTACT_ROT': StellarClasses.CONTACT_ROT,
-				'APERIODIC': StellarClasses.APERIODIC,
-				'CONSTANT': StellarClasses.CONSTANT
-			}
+		data = np.genfromtxt(os.path.join(self.input_folder, 'targets.txt'),
+			dtype=None,
+			delimiter=',',
+			comments='#',
+			encoding='utf-8')
 
-			# Create list of all the classes for each star:
-			lookup = []
-			for rowidx, row in enumerate(data):
-				#starid = int(row[0][4:])
+		# Create list of all the classes for each star:
+		lookup = []
+		for rowidx, row in enumerate(data):
+			if rowidx in self.test_idx:
 				labels = row[1].strip().split(';')
-				lbls = []
-				for lbl in labels:
-					lbl = lbl.strip()
-					c = translate.get(lbl.strip())
-					if c is None:
-						logger.error("Unknown label: %s", lbl)
-					else:
-						lbls.append(c)
+				lbls = [StellarClasses[lbl.strip()] for lbl in labels]
+				lookup.append(tuple(set(lbls)))
 
-				if rowidx in self.test_idx:
-					lookup.append(tuple(set(lbls)))
-			return tuple(lookup)
+		return tuple(lookup)

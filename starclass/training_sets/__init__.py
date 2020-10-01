b72bff68e27ff6e09cd1745bf6cde255c6df05d5
Rasmus Handberg <rasmush@phys.au.dk>
2020-10-01 11:38:48 +0200

    Bugfix
    Bug from previous merge

--- starclass/training_sets/__init__.py
+++ starclass/training_sets/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # flake8: noqa
 
-from .training_set import TrainingSet, tset_available
+from .training_set import TrainingSet
 from .tda_simulations import tda_simulations
 from .keplerq9 import keplerq9
 from .keplerq9linfit import keplerq9linfit

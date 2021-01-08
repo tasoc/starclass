b39e1b3ccef547a8464a828deae9103e4e890fe0
Rasmus Handberg <rasmush@phys.au.dk>
2021-01-08 14:40:04 +0100

    Bugfix for documentation

--- starclass/constants.py
+++ starclass/constants.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Training set convenience functions.
+Constants used throughout package.
 
 .. codeauthor:: Rasmus Handberg <rasmush@phys.au.dk>
 """
 
 #--------------------------------------------------------------------------------------------------
-"""
-List of available classifiers.
-"""
+#: List of available classifiers.
 classifier_list = ('rfgc', 'slosh', 'xgb', 'sortinghat', 'meta')
 
 #--------------------------------------------------------------------------------------------------
-"""
-List of available training sets.
-"""
-trainingset_list = ('keplerq9v3', 'keplerq9v3-instr', 'keplerq9v2', 'keplerq9', 'tdasim', 'tdasim-raw', 'tdasim-clean')
+#: List of available training sets.
+trainingset_list = (
+	'keplerq9v3',
+	'keplerq9v3-instr',
+	'keplerq9v2',
+	'keplerq9',
+	'tdasim',
+	'tdasim-raw',
+	'tdasim-clean'
+)

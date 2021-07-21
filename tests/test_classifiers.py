9317079ac2816679b4b6089b590d09ebf6272ced
Rasmus Handberg <rasmush@phys.au.dk>
2021-07-21 21:31:59 +0200

    More testing, including MPI

--- tests/test_classifiers.py
+++ tests/test_classifiers.py
@@ -16,6 +16,12 @@ from conftest import capture_run_cli
 import starclass
 from starclass.training_sets.testing_tset import testing_tset
 
+try:
+	import mpi4py # noqa: F401
+	MPI_AVAILABLE = True
+except ImportError:
+	MPI_AVAILABLE = False
+
 AVALIABLE_CLASSIFIERS = list(starclass.classifier_list)
 AVALIABLE_CLASSIFIERS.remove('meta')
 
@@ -67,6 +73,7 @@ def test_run_training(PRIVATE_INPUT_DIR, classifier):
 		logfile = os.path.join(tmpdir, 'training.log')
 		todo_file = os.path.join(PRIVATE_INPUT_DIR, 'todo_run.sqlite')
 
+		# Train the classifier:
 		out, err, exitcode = capture_run_cli('run_training.py', [
 			'--classifier=' + classifier,
 			'--trainingset=testing',
@@ -81,45 +88,70 @@ def test_run_training(PRIVATE_INPUT_DIR, classifier):
 		# Check that a log-file was indeed generated:
 		assert os.path.isfile(logfile), "Log-file not generated"
 
-		out, err, exitcode = capture_run_cli('run_starclass.py', [
-			'--debug',
-			'--classifier=' + classifier,
-			'--trainingset=testing',
-			'--level=L1',
-			'--datadir=' + tmpdir,
-			todo_file
-		])
-		assert exitcode == 0
+		# We now have a trained classifier, so we should be able to run the classification:
+		for mpi in ([False, True] if MPI_AVAILABLE else [False]):
+			cli = 'run_starclass_mpi.py' if mpi else 'run_starclass.py'
+			out, err, exitcode = capture_run_cli(cli, [
+				'--debug',
+				'--overwrite',
+				'--classifier=' + classifier,
+				'--trainingset=testing',
+				'--level=L1',
+				'--datadir=' + tmpdir,
+				todo_file
+			], mpiexec=mpi)
+			assert exitcode == 0
 
-		# Do a deep inspection of the todo-file:
-		with closing(sqlite3.connect('file:' + todo_file + '?mode=ro', uri=True)) as conn:
-			conn.row_factory = sqlite3.Row
-			cursor = conn.cursor()
+			# Do a deep inspection of the todo-file:
+			with closing(sqlite3.connect('file:' + todo_file + '?mode=ro', uri=True)) as conn:
+				conn.row_factory = sqlite3.Row
+				cursor = conn.cursor()
 
-			cursor.execute("SELECT * FROM starclass_settings;")
-			row = cursor.fetchall()
-			assert len(row) == 1, "Only one settings row should exist"
-			settings = row[0]
-			print(dict(settings))
-			assert settings['tset'] == 'testtset'
+				cursor.execute("SELECT * FROM starclass_settings;")
+				row = cursor.fetchall()
+				assert len(row) == 1, "Only one settings row should exist"
+				settings = row[0]
+				print(dict(settings))
+				assert settings['tset'] == 'testtset'
 
-			cursor.execute("SELECT * FROM starclass_diagnostics WHERE priority=17;")
-			row = cursor.fetchone()
-			print(dict(row))
-			assert row['priority'] == 17
-			assert row['classifier'] == classifier
-			assert row['status'] == starclass.STATUS.OK.value
-			assert row['errors'] is None
-
-			cursor.execute("SELECT * FROM starclass_results;")
-			results = cursor.fetchall()
-			assert len(results) == len(tset.StellarClasses)
-			for row in cursor.fetchall():
+				cursor.execute("SELECT * FROM starclass_diagnostics WHERE priority=17;")
+				row = cursor.fetchone()
 				print(dict(row))
 				assert row['priority'] == 17
 				assert row['classifier'] == classifier
-				tset.StellarClasses[row['class']] # Will result in KeyError of not correct
-				assert 0 <= row['prob'] <= 1, "Invalid probability"
+				assert row['status'] == starclass.STATUS.OK.value
+				assert row['errors'] is None
+
+				cursor.execute("SELECT * FROM starclass_results;")
+				results = cursor.fetchall()
+				assert len(results) == len(tset.StellarClasses)
+				for row in cursor.fetchall():
+					print(dict(row))
+					assert row['priority'] == 17
+					assert row['classifier'] == classifier
+					tset.StellarClasses[row['class']] # Will result in KeyError of not correct
+					assert 0 <= row['prob'] <= 1, "Invalid probability"
+
+				cursor.execute("SELECT * FROM starclass_features_common;")
+				results = cursor.fetchall()
+				assert len(results) == 1
+				row = dict(results[0])
+				print(row)
+				assert row['priority'] == 17
+				assert len(row) > 1
+
+				if classifier != 'slosh':
+					cursor.execute(f"SELECT * FROM starclass_features_{classifier:s};")
+					results = cursor.fetchall()
+					assert len(results) == 1
+					row = dict(results[0])
+					print(row)
+					assert row['priority'] == 17
+					assert len(row) > 1
+				else:
+					# For SLOSH the table should not exist:
+					cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='starclass_features_{classifier:s}';")
+					assert len(cursor.fetchall()) == 0
 
 #--------------------------------------------------------------------------------------------------
 if __name__ == '__main__':

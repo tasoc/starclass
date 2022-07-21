a3e54be1b9ef531e843c4ee973b95e3e27acd179
Rasmus Handberg <rasmush@phys.au.dk>
2022-07-21 21:08:53 +0200

    Filter lightcurves on quality flags during load

--- tests/test_io.py
+++ tests/test_io.py
@@ -7,6 +7,7 @@
 import pytest
 import os.path
 import tempfile
+import lightkurve as lk
 import conftest # noqa: F401
 from starclass import io
 
@@ -64,6 +65,21 @@ def test_json():
 		recovered_object = io.loadJSON(fname + ".gz")
 		print(recovered_object)
 		assert test_object == recovered_object, "The object was not recovered"
+
+#--------------------------------------------------------------------------------------------------
+@pytest.mark.parametrize('fname,mission', [
+	['tess00029281992-s01-c1800-dr01-v04-tasoc-cbv_lc.fits.gz', 'TESS'],
+	['kplr001864183-2012179063303_llc.fits.gz', 'Kepler'],
+])
+def test_load_lightcurve(SHARED_INPUT_DIR, fname, mission):
+
+	fpath = os.path.join(SHARED_INPUT_DIR, 'create_todolist', fname)
+	assert os.path.isfile(fpath), "File does not exist"
+
+	lc = io.load_lightcurve(fpath)
+
+	assert isinstance(lc, lk.LightCurve)
+	assert lc.mission == mission
 
 #--------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
